e2870296ada5c7a605c7c3eaeb7d403771c277ca
Jayadhiman31 <101348878+Jayadhiman31@users.noreply.github.com>
2024-05-10 00:30:59 +0530

    bfs.py
    breadth first search in python

---
+++ bfs.py
@@ -0,0 +1,22 @@
+from collections import deque
+def BFS(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.add(node)
+            print(node, end=" ")
+            queue.extend(graph[node])
+
+graph = {
+    "A": ["B", "C"],
+    "B": ["A", "D", "E"],
+    "C": ["A", "F"],
+    "D": ["B"],
+    "E": ["B", "F"],
+    "F": ["C", "E"],
+}
+
+BFS(graph, "A")
